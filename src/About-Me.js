0a9ef9182f12f8b53966e92c8abc3e8ecb1c6a2e
austinbhan <austinbhan@outlook.com>
2022-07-22 16:02:41 -0700

    Redesign about me

--- src/About-Me.js
+++ src/About-Me.js
@@ -8,23 +8,22 @@ export default function AboutMe() {
 
   return (
     <div className="about-me">
-      <div className="text">
-        <p>I&apos;m a full stack software engineer based in Portland, OR. 
+      <img className="profile-pic" src="/images/profile-pic.jpeg"/>
+      <p className="text">I&apos;m a full stack software engineer based in Portland, OR. 
           I was previously a classical musician based in Kentucky, Indiana, Michigan, and California.
           I have no formal drawing experience.
           Drawings are inspired by personal experiences, 
           the interests and personalities of my colleagues,
           or nothing at all. My style is mostly inspired by Sanrio.
-          I presently do not have plans to monetize my work, but will accept requests and ideas.
-        </p>
-        <p>
+      </p>
+      <p className="text">
         All drawings are made with Procreate.
         Site hosting by Netlify, with database support from Supabase and Imgur.
-        Site features will increase as I continue to learn.  
-        </p>
-        <p>
-          <a href="https://github.com/austinbhan" target="_blank" rel="noreferrer">My Github</a>
-        </p>
+        More site features will be added as I continue to learn.  
+      </p>
+      <div>
+        <a href="https://github.com/austinbhan" target="_blank" rel="noreferrer"><img className="logos" src="/images/github.png" /></a>
+        <a href="https://www.linkedin.com/in/austin-han-740a69157/" target="_blank" rel="noreferrer"><img className="logos" src="/images/linkedin.png"/></a>
       </div>
       <button className="button" onClick={handleGoBack}>Take me back</button>
     </div>
